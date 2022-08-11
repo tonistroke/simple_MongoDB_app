887293aba482d165783f623229832d4dabc4f191
Anthony Lopez <51010799+tonistroke@users.noreply.github.com>
2022-08-11 15:59:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+"""
+Crear una aplicación de línea de comandos que permita 
+1. registro
+2. búsqueda
+3. edición
+4. eliminación
+de artículos dentro de un sistema de inventario."""
+
 import pymongo
 from pymongo import MongoClient
 
@@ -8,98 +16,88 @@ cluster = MongoClient("mongodb+srv://toni:1234@cluster0.wi9ojnj.mongodb.net/?ret
 db = cluster["FirstMongoDBApp"]
 collection = db["Diccionario"]
 
-#Añadiendo vocabulario--------------------------------------------
-post1 = {"word":"Abuelazón", "meaning":"Dícese de la conducta de entusiasmo excesivo que los abuelos sienten por los nietos; actitud típica de personas ancianas."}
-post2 = {"word":"Ahuevado", "meaning":"sinónimo de huevón, lento, imbécil"}
-post3 = {"word":"Arrabalero/a", "meaning":"Buscapleitos"}
-post4 = {"word":"Arranque", "meaning":"de arrancarse [irse de fiesta/a parrandear]"}
-post5 = {"word":"Ayala", "meaning":"interjección de sorpresa o enojo. "}
-post6 = {"word":"Cabreado(a)", "meaning":"Que esta molesto(a)."}
-post7 = {"word":"Cafá", "meaning":"Una Palmada fuerte átras en la cabeza."}
-post8 = {"word":"Carretilla", "meaning":"Que va muy rapído e.j. A carretilla, como una carretilla..."}
-post9 = {"word":"Camarón", "meaning":"actividad extracurricular que permite a un individuo ganar dinero extra."}
-post10 = {"word":"Chantin", "meaning":"casa, hogar."}
-post11 = {"word":"Cholywood", "meaning":"farándula panameña."}
-post12 = {"word":"Datien", "meaning":"  "}
-post13 = {"word":"De a vaina", "meaning":"ganar algo por pura buena suerte en el ultimo momento."}
-post14 = {"word":"En bomba", "meaning":"objeto atractivo (no se usa con personas), bueno, excelente"}
-post15 = {"word":"Frenteao", "meaning":"Decir directamente a uno, en la cara"}
-post16 = {"word":"Fulo(a)", "meaning":"Rubio/a."}
-post17 = {"word":"Fundillo", "meaning":"especificamente el orificio anal."}
-post18 = {"word":"Fuas", "meaning":"Nalgas, Trasero."}
-post19 = {"word":"Fuste", "meaning":"Sinonimo de fuas."}
-post20 = {"word":"Gallinero", "meaning":" la entreda general o area popular de algun evento cultural (concierto) o evento deportivo."}
-post21 = {"word":"Grubeo/ar", "meaning":"Estar con una persona por un tiempo o por una noche para pasarla bien y para nada serio o formal."}
-post22 = {"word":"Guagua", "meaning":"se dice de un automóvil muy viejo o en mal estado."}
-post23 = {"word":"Yeyé", "meaning":"Persona adinerada que presume de su condición y/o comúnmente usa anglicismos en su habla."}
-post24 = {"word":"Zambito(a)", "meaning":"expresión de las provincias de Herrera y Los Santos, significa niño o niña."}
 
-collection_insert_many(post1, post2, post3, post4, post5,
-                      post6, post7, post8, post9, post10,
-                      post11, post12, post13, post14, post15,
-                      post16, post17, post18, post19, post20,
-                      post21, post22, post23, post24)
+#AddingArticles-------------------------------------------------------------------
 
-#Funciones--------------------------------------------------------
+#ENTRADA = CÓDIGO PRODUCTO,	DESCRIPCIÓN,	ENTRADAS,	SALIDAS,	STOCK
+#TOTAL
 
-def add_word(In):
-  result= collection.insert_one(In)
-  print(result)
-  return result
+entrada00 = {"Codigo_de_producto":1, "Descripcion":"Espárragos", "Entradas":19, "Salidas":28,	"Stock":76}
+entrada01 = {"Codigo_de_producto"2, "Descripcion":"Alcachofas", "Entradas":31,	"Salidas":39,	"Stock":72}
+entrada02 = {"Codigo_de_producto":3, "Descripcion":"Pimientos del piquillo", "Entradas":16, "Salidas":14,	"Stock":57}
+entrada03 = {"Codigo_de_producto":4, "Descripcion":"Tomates", "Entradas":9, "Salidas":12, "Stock":37}
+entrada04 = {"Codigo_de_producto":5, "Descripcion":"Guisantes", "Entradas":11, "Salidas":6, "Stock":25}
+entrada05 = {"Codigo_de_producto":6, "Descripcion":"Judías", "Entradas":7, "Salidas":6, "Stock":21}
 
-def change_word(In, In1)
-  result = collection.update_one{"word":In}, { $set: {"word":In1} }
-  return result
-
-def delete_a_word(In):
-  result = collection.delete_one(In)
-  return result
-
-def look_for_meaning(In):
-  result = collection.find_one("word":In)
-  print(result)
+collection_insert_many(entrada00, entrada01, entrada02, entrada03, entrada04, entrada05)
 
 
-#Programa principal-----------------------------------------------
+#Functions-----------------------------------------------------------------------
 
-print("_________________Diccionario slang panameño_________________")
-do {
-print("""
-    Elegir una de las siguientes opciones:
-        1.- Añadir nueva palabra
-        2.- Cambiar palabra existente
-        3.- Eliminar palabra del diccionario
-        4.- Mostrar todas las palabras con su significado
-        5.- Buscar el significado de una palabra
-""")
-num = input()
-if num == 1:
-  print("Añadir nueva palabra")
+def add(In):
+  r = collection.insert_one(In)
+  print(r)
+  return r
 
-  palabra = input("palabra: ")
-  significado = input("significado: ")
-  pal = {"word":palabra, "meaning":significado}
-  add_word(pal)
-elif num == 2:
-  print("Cambiar palabra existente")
-  palabra2 = input("palabra a cambiar: ")
-  nuevaPalabra = input("nueva palabra: ")
-  change_word(palabra2,nuevaPalabra)
-elif num == 3:
-  print("Eliminar palabra del diccionario")
-  palabra3 = input("palabra: ")
-  pal3 = {"word":palabra3}
-  delete_a_word(pal3)
-elif num == 4:
-  print("Mostrar todas las palabras con su significado")
-  result4 = collection.find()
-  print(result4)
-elif num == 5:
-  print("Buscar el significado de la siguiente palabra")
-  palabra5 = input("palabra:")
-  show5 = {"word":palabra5, "meaning"}
-  print(show)
-elif:
-  print("El programa se reiniciara")
+def find(In):
+  r = collection.find_one("Codigo_de_producto":In)
+  print(r)
 
-}while(num>0);
+def delete(In):
+  r = collection.delete_one(In)
+  return r
+
+
+#MainApp--------------------------------------------------------------------------
+print("INVENTARIO MENSUAL DE PRODUCTOS")
+do{
+  print("""
+  ________________________MENU________________________
+          1. Registrar nuevo articulo
+          2. Buscar un articulo
+          3. Editar un articulo
+          4. Eliminar un articulo
+  """)
+  i = input("Elegir una de las 4 opciones 1, 2, 3 o 4:")
+  
+  if i==1:
+    print("Registrar nuevo articulo")
+    cdp = input("Codigo del producto: ")
+    dscr = input("Descripcion del producto: ")
+    entr = input("Entradas mensuales del producto: ")
+    slds = input("Salidas mensuales del producto: ")
+    stck = input("Stock actual del producto: ")
+
+    entrada = {"Codigo_de_producto":cdp,
+               "Descripcion":dscr, 
+               "Entradas":entr, 
+               "Salidas":slds, 
+               "Stock":stck}
+
+    add(entrada)
+
+  if i==2:
+    print("Buscar un articulo")
+    articulo = input("Ingresar codigo del producto: ")
+    
+    find(articulo)
+
+  if i==3:
+    print("Editar un articulo")
+    cdp = input("Actualizar codigo del producto: ")
+    dscr = input("Actualizar descripcion del producto: ")
+    entr = input("Actualizar entrada mensual del producto: ")
+    slds = input("Actualizar salida mensual del producto: ")
+    stck = input("Actualizar stock actual del producto: ")
+    r = collection.update_one{"Codigo_de_producto":cdp,
+                              "Descripcion":dscr, 
+                              "Entradas":entr, 
+                              "Salidas":slds, 
+                              "Stock":stck}
+    
+  if i==4:
+    print("Eliminar un articulo")
+    codigo = input("Codigo de producto a eliminar: ")
+    articulo = {"Codigo_de_producto":codigo}
+    delete(articulo)
+}while(i>0)
